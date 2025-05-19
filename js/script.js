f036705e13677905aaf4f3a14d5fb791eb1ec4d7
wakmaia <wakoleva.maia@gmail.com>
2025-05-19 08:19:40 +0200

    wip: add tip calculator

--- js/script.js
+++ js/script.js
@@ -6,20 +6,29 @@ let customBtn = document.getElementById("customBtn");
 let customInput = document.getElementById("customInput");
 let total= document.getElementById("total");
 let errorMsg = document.getElementById("no-zero")
+let form = document.querySelector("form");
 
 let selectedTipPercent = 0;
 
 //Change button to input when clicked
 customBtn.addEventListener("click", function () {
   customBtn.style.display = "none";
+  customInput.style.display = "inline-block"; 
   customInput.focus();
 });
+
+//Show custon button when clicked reset
+form.addEventListener("reset", function () {
+  customBtn.style.display = "inline-block";
+  customInput.style.display = "none";
+ })
 
 customInput.addEventListener("input", function () {
   selectedTipPercent = parseFloat(customInput.value);
   calculateTip();
 });
 
+//Select % tip
 for (var i = 0; i < tipPercent.length; i++) {
   tipPercent[i].addEventListener("click", function () {
     //remove selected class on clicked button
@@ -28,8 +37,11 @@ for (var i = 0; i < tipPercent.length; i++) {
       console.log(tipPercent[i]);
       }
 
+
     //add selected class on clicked button
     this.classList.add("selected");
+
+    selectedTipPercent = parseFloat(this.textContent.replace('%', ''));
 
     calculateTip();
   });
@@ -42,21 +54,30 @@ function calculateTip() {
   const bill = parseFloat(billInput.value);
   const people = parseInt(peopleInput.value);
 
-  if (bill || people || tipPercent) {
+  if (isNaN(bill) || isNaN(people) || people === 0 || isNaN(selectedTipPercent)) {
+  
+    if (people === 0) {
+      peopleInput.classList.add("error"); 
+      peopleInput.style.border = "1.5px solid red";
+      errorMsg.textContent = "Can't add zero"
+    } else {
+      errorMsg.textContent = "";
+      peopleInput.style.border = "";
+      peopleInput.classList.remove("error");
+    }
+    return;
+  }
+
+    // tip %
     let tip = bill * (selectedTipPercent / 100);
 
     //Tip per person
     let tipPerPerson = tip / people;
-    tipAmount.textContent = `$ ${tipPerPerson.toFixed(2)};`;
+    tipAmount.textContent = `$ ${tipPerPerson.toFixed(2)}`;
     console.log("tipPerPerson2", tipPerPerson);
 
     //Total per person
     totalPerPerson = (bill + tip) / people;
-    total.textContent = `$ ${totalPerPerson.toFixed(2)};`;
+    total.textContent = `$ ${totalPerPerson.toFixed(2)}`;
   }
-  if (people === 0) {
-    peopleInput.classList.add("error"); 
-    peopleInput.style.border = "1.5px solid red";
-    errorMsg.textContent = "Can't add zero"
-  } 
-}
+
